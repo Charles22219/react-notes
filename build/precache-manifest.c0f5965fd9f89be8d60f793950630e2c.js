88b0cc4cc008c9a194a4714a793c47c670ff27e2
Charles_Cabasal <interns@bitnacs.com>
2020-10-26 00:13:42 +0800

    added build for frontend

---
+++ build/precache-manifest.c0f5965fd9f89be8d60f793950630e2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ce83eadd806d5fbcda70d2c39f6750",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6cb97e8f3056cc3a878",
+    "url": "/static/js/2.5463c031.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.5463c031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675a81bc2197e313fd9e",
+    "url": "/static/js/main.72321e0a.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
