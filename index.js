c4e308b1b325ae31d0a96d15b051e6b2e617b023
Charles_Cabasal <interns@bitnacs.com>
2020-10-29 18:02:07 +0800

    changed index

--- index.js
+++ index.js
@@ -1,55 +1,17 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+require('dotenv').config()
 const Note = require('./models/note')
 
-const url = process.env.MONGODB_URI
+const cors = require('cors')
 
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-
-app.use(requestLogger)
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+app.use(express.static('build'))
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
-    response.json(notes)
+    response.json(notes.map(note => note.toJSON()))
   })
 })
 
@@ -67,28 +29,65 @@ app.post('/api/notes', (request, response) => {
   })
 
   note.save().then(savedNote => {
-    response.json(savedNote)
+    response.json(savedNote.toJSON())
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    note ? response.json(note) : response.status(404).end()
-  }).catch(error => {
-    console.log(error);
-    response.status(500).end()
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note.toJSON())
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
 
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT 
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
